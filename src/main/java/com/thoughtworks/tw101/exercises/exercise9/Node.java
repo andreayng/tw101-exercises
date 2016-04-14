acaf3b3cdc3bf5be1f8f781704109fc1949ef303
andreayng <andrea.y.ng@gmail.com>
2016-04-14 00:29:35 -0400

    Completed Exercise 9 - implement a Node class for a binary tree with add() and names() methods to add nodes and print the nodes in the tree

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,41 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node leftChild, rightChild;
 
     public Node(String name) {
         this.name = name;
+        leftChild = null;
+        rightChild = null;
     }
 
     public void add(String nameOfNewNode) {
+        int stringOrder = name.compareTo(nameOfNewNode);
+        if(stringOrder >= 0) {
+            if(leftChild == null)
+                leftChild = new Node(nameOfNewNode);
+            else
+                leftChild.add(nameOfNewNode);
+        }
+        else {
+            if(rightChild == null)
+                rightChild = new Node(nameOfNewNode);
+            else
+                rightChild.add(nameOfNewNode);
+        }
     }
 
     public List<String> names() {
-        return null;
+        List<String> nameList = new ArrayList<>();
+        if(leftChild != null)
+            nameList.addAll(leftChild.names());
+        nameList.add(name);
+        if(rightChild != null)
+            nameList.addAll(rightChild.names());
+        return nameList;
     }
 }
